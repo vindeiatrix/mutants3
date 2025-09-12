d6d586ae174051ede9b1e81bd2a1bb990de390b7
vindeiatrix <michaelbowmantech@yahoo.com>
2025-09-11 21:36:49 -0400

    Canonicalize ground item names

--- src/mutants/ui/viewmodels.py
+++ src/mutants/ui/viewmodels.py
@@ -24,7 +24,7 @@ class RoomVM(TypedDict, total=False):
     coords: Coords
     dirs: Dict[str, EdgeDesc]
     monsters_here: List[Thing]
-    ground_items: List[str]
+    ground_item_ids: List[str]
     has_ground: bool
     events: List[str]
     shadows: List[str]
